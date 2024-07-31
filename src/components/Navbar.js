1891a6d622258c95e4f34bdb18bf1b49cc1ce6b8
surajraj95066 <166308823+surajraj95066@users.noreply.github.com>
2024-07-31 20:47:43 +0530

    Your descriptive commit message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
+
 export default function Navbar() {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
                 <a className="navbar-brand" href="https://surajraj95066.github.io/project-react/">FoldHope</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active" style={{ marginRight: '15px' }}>
-                            <Link to="/Working" style={{ color: 'white', textDecoration: 'none' }}>Procedure</Link>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item active">
+                            <Link to="/Working" className="nav-link">Procedure</Link>
                         </li>
-                        <li className="nav-item" style={{ marginRight: '15px' }}>
-                            <Link to="/Data1" style={{ color: 'white', textDecoration: 'none' }}>Survey Report</Link>
+                        <li className="nav-item">
+                            <Link to="/Data1" className="nav-link">Survey Report</Link>
                         </li>
-
                     </ul>
                 </div>
-            </nav>
-        </div>
-    )
+            </div>
+        </nav>
+    );
 }
