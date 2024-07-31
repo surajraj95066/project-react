27795dd86b27b0857fdc95c1122f1292ef4a7af2
surajraj95066 <166308823+surajraj95066@users.noreply.github.com>
2024-07-31 17:11:30 +0530

    Your descriptive commit message

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App'; // Ensure this is the only declaration of App
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
 import Working from './components/Working';
 import Data1 from './components/Data1';
+import FoldHope from './components/FoldHope';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Working" element={<Working />} />
         <Route path="/Data1" element={<Data1 />} />
+        <Route path="/FoldHope" element={<FoldHope />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
